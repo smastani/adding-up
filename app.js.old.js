a1e48eed6698274a26307980ff8d8ec83c2eddbe
Satoshi Masutani <smastani@w2.dion.ne.jp>
2019-10-09 23:58:17 +0900

    ランキングの実装完了

--- app.js.old.js
+++ app.js.old.js
@@ -7,8 +7,8 @@ const prefectureDataMap = new Map(); // key: 都道府県 value: 集計データ
 rl.on('line', (lineString) => {
   const columns = lineString.split(',');
   const year = parseInt(columns[0]);
-  const prefecture = columns[2];
-  const popu = parseInt(columns[7]);
+  const prefecture = columns[1];
+  const popu = parseInt(columns[3]);
   if (year === 2010 || year === 2015) {
     let value = prefectureDataMap.get(prefecture);
     if (!value) {
@@ -28,14 +28,15 @@ rl.on('line', (lineString) => {
   }
 });
 rl.on('close', () => {
-  for (let [key, value] of prefectureDataMap) { 
-    value.change = value.popu15 / value.popu10;
-  }
-  const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change;
+    for (let [key, value] of prefectureDataMap) { 
+      value.change = value.popu15 / value.popu10;
+    }
+    const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
+      return pair2[1].change - pair1[1].change;
+    });
+    const rankingStrings = rankingArray.map(([key, value]) => {
+      return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
+    });
+    console.log(rankingStrings);
   });
-  const rankingStrings = rankingArray.map(([key, value]) => {
-    return key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change;
-  });
-  console.log(rankingStrings);
-});
+
